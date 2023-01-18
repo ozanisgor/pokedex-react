2b1cafc4410a00d0a0bad3350c773f5c462c081b
ozan isgor <ozanisgor11@gmail.com>
2023-01-18 15:56:27 +0300

    add: Pages & Routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Pokedex</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search Pokemons and see details.
+      </p>
+    </>
+  )
+}
+export default About
